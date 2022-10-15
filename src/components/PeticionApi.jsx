26f9a850ca8c7261d2ab1af946b464f0789bf4a3
Darwin Garcia <soporte.darwin.garcia@gmail.com>
2022-10-15 15:32:17 -0500

    Enlazando archivo PeticionApi

---
+++ src/components/PeticionApi.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const PeticionesApi = () =>{
+    
+
+
+    return (
+        <>
+            <h1>PETICIÓN AL API DE MARVEL</h1>
+            
+            <div class="paginator">
+              
+              
+                    
+              
+            </div>
+        
+            
+            </>
+      )
+}
+
+export default PeticionesApi
